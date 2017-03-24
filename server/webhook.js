29053bfa99cc9b4ec72ef4027bc68e4a7831436d
Romain Dardour <romain@hull.io>
2017-03-24 16:57:48 +0100

    cleanup

--- server/webhook.js
+++ server/webhook.js
@@ -1,23 +1,25 @@
-import _ from 'lodash';
 import axios from 'axios';
 
-export default function webhook({ webhooks_urls, hull, payload = {} }) {
-  return _.map(webhooks_urls, url => axios.post(url, payload)
-    .then(
-      ({ data, status, statusText }) => hull.logger.info('webhook.success', {
-        userId: payload.user.id,
-        status,
-        statusText,
-        data
-      }))
-    .catch(({ response, message: msg }) => {
-      if (response) {
-        const { data, status } = response;
-        hull.logger.info('webhook.error', { message: 'webhook failed', data, status });
-      } else {
-        // Something happened in setting up the request that triggered an Error
-        hull.logger.error('webhook.error', { message: msg });
-      }
-    })
-  );
+export default function webhook({ madkudu_key= "", hull, payload = {} }) {
+  return axios({
+    method: "post",
+    url: "https://api.madkudu.com/v1/segment",
+    data: payload
+  })
+  .then(
+    ({ data, status, statusText }) => hull.logger.info('madkudu.success', {
+      userId: payload.user.id,
+      status,
+      statusText,
+      data
+    }))
+  .catch(({ response, message: msg }) => {
+    if (response) {
+      const { data, status } = response;
+      hull.logger.info('madkudu.error', { message: 'madkudu failed', data, status });
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      hull.logger.error('madkudu.error', { message: msg });
+    }
+  })
 }
